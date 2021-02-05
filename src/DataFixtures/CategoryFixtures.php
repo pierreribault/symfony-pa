0ef401f29005d02689c4877a53c85e2be5ac943e
florian <flopau53@gmail.com>
2021-02-05 20:17:21 +0100

    Add Fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -4,7 +4,24 @@
 namespace App\DataFixtures;
 
 
-class CategoryFixtures
+use App\Entity\Category;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class CategoryFixtures extends AbstractFixture
 {
 
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $category = (new Category())->setName($faker->word)->setType($faker->word);
+            $manager->persist($category);
+            $manager->flush();
+        }
+    }
 }
--- src/DataFixtures/CompanyFixtures.php
+++ src/DataFixtures/CompanyFixtures.php
@@ -4,7 +4,32 @@
 namespace App\DataFixtures;
 
 
-class CompanyFixtures
+use App\Entity\Company;
+use App\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class CompanyFixtures extends AbstractFixture implements DependentFixtureInterface
 {
 
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        /** @var User $userCompany */
+        $userCompany = $manager->getRepository(Company::class)->findOneBy(["roles" => ["ROLE_COMPANY"]]);
+
+        $company = (new Company())->setName($faker->word)->setAccount($userCompany)->setSiret($faker->word)->setSiteUrl($faker->url)->setDescription($faker->sentence);
+
+        $manager->persist($company);
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [UserFixtures::class];
+    }
 }
