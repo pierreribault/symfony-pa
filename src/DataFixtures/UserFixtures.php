14a53945c5b51843a9ba0b61111f96a10c252399
florian <flopau53@gmail.com>
2021-02-05 20:17:21 +0100

    Add Fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,7 +4,50 @@
 namespace App\DataFixtures;
 
 
-class UserFixtures
+use App\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class UserFixtures extends AbstractFixture
 {
 
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        $user = (new User())
+            ->setEmail($faker->email)
+            ->setAddress($faker->address)
+            ->setEnabled(true)
+            ->setIsVerified(true)
+            ->setPassword("root")
+            ->setPhone($faker->phoneNumber)
+        ;
+        $company = (new User())
+            ->setEmail($faker->email)
+            ->setAddress($faker->address)
+            ->setEnabled(true)
+            ->setIsVerified(true)
+            ->setPassword("root")
+            ->setPhone($faker->phoneNumber)
+            ->setRoles(["ROLE_COMPANY"])
+        ;
+
+        $admin = (new User())
+            ->setEmail($faker->email)
+            ->setAddress($faker->address)
+            ->setEnabled(true)
+            ->setIsVerified(true)
+            ->setPassword("root")
+            ->setPhone($faker->phoneNumber)
+            ->setRoles(["ROLE_ADMIN"]);
+
+       $manager->persist($user);
+       $manager->persist($company);
+       $manager->persist($admin);
+
+        $manager->flush();
+    }
 }
