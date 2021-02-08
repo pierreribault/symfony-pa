fd95eeef6a98266b7fb921c6ef68419e0912502a
florian <flopau53@gmail.com>
2021-02-08 14:03:31 +0100

    Feat: -start Contact form

---
+++ src/Controller/Front/ContactController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/contact")
+ */
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/", name="contact_index", methods={"GET"})
+     * @param ContactRepository $contactRepository
+     * @return Response
+     */
+    public function index(Request $request, ContactRepository $contactRepository): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('contact_index');
+        }
+
+        return $this->render('contact/new.html.twig', [
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/{id}", name="contact_show", methods={"GET"})
+     * @param Contact $contact
+     * @return Response
+     */
+    public function show(Contact $contact): Response
+    {
+        return $this->render('contact/show.html.twig', [
+            'contact' => $contact,
+        ]);
+    }
+}
