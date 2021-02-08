2c2f862f442e7f3a6bc3b19f186fdd849c43d65e
florian <flopau53@gmail.com>
2021-02-08 14:03:31 +0100

    Feat: -start Contact form

---
+++ src/Form/ContactType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('subject')
+            ->add('email')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('message')
+            ->add('createdAt')
+            ->add('company')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
