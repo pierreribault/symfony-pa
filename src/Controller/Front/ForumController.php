eef352b16088f57bac935f4c1059f6acd6469f64
Maeva <maeva@uxia-agency.com>
2021-06-11 17:59:23 +0200

    faille xss, searc forum (wip)

--- src/Controller/Front/ForumController.php
+++ src/Controller/Front/ForumController.php
@@ -8,16 +8,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ForumController extends AbstractController
 {
     /**
-     * @Route("/forum", name="forum")
+     * @Route("/forum", name="forum", methods={"GET", "POST"})
      */
     public function forum(Request $request, PaginatorInterface $paginator, ForumThreadRepository $forumThreadRepository)
     {
 
         $data = $forumThreadRepository->findBy([],['createdAt' => 'desc']);
+
+        // SEARCH
+        $form = $this->createFormBuilder(null)
+            ->add('search', SearchType::class, [
+                'attr' => [
+                    'class' => 'border-silver border rounded-full hover:border-eden',
+                    'placeholder' => 'Search'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'text-silver'
+                ]
+            ])
+            ->getForm();
 
         $threads = $paginator->paginate(
             $data,
@@ -26,7 +44,8 @@ class ForumController extends AbstractController
         );
 
         return $this->render('front/forum/forum.html.twig', [
-            'threads' => $threads
+            'threads' => $threads,
+            'search_form' => $form->createView()
         ]);
     }
 }
